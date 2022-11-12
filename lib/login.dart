78a5d74c5be12ab7ae07d5265d49cfddfaf9669b
Mubidotnet <mubashiri639@gmail.com>
2022-11-13 03:40:57 +0500

    task1

---
+++ lib/login.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class loginScreen extends StatefulWidget {
+  const loginScreen({super.key});
+
+  @override
+  State<loginScreen> createState() => _loginScreenState();
+}
+
+class _loginScreenState extends State<loginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.purple,
+          title: Text('Login'),centerTitle: true,),
+    body: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+      TextFormField(
+  decoration: const InputDecoration(
+      border: UnderlineInputBorder(),
+      labelText: 'Enter your email',
+  ),
+      ),
+      SizedBox(height: 40,),
+  Center(
+      child: TextFormField(
+      obscureText:true
+    ,
+        decoration: const InputDecoration(
+          border: UnderlineInputBorder(),
+          labelText: 'Enter Password',
+        ),
+      ),
+  ),
+  SizedBox(height: 40,),
+  ElevatedButton(onPressed: (){}, child:Text('Login'))
+      
+
+
+      ],),
+    ),
+    );
+  }
+}
